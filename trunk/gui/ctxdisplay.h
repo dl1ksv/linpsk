c1232f560fe3927238ac74de37ec374c862acb35
dl1ksv <dl1ksv@3a9df27d-fc75-45e3-aa45-fb3d8ea6d1dd>
2012-10-26 07:39:58 +0000

    preparing 1.2

---
+++ trunk/gui/ctxdisplay.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+                          |FILENAME|  -  description
+                             -------------------
+    begin                : |DATE|
+    copyright            : (C) |YEAR| by |AUTHOR|
+    email                : |EMAIL|
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   The PSK part is based on WinPSK 1.0 by Moe Wheatley, AE4JY            *
+ ***************************************************************************/
+
+#ifndef CTXDISPLAY_H
+#define CTXDISPLAY_H
+
+
+#include <QFrame>
+#include "ui_ctxdisplay.h"
+
+class CTxDisplay : public QFrame, public Ui::CTxDisplay
+
+{
+    Q_OBJECT
+
+public:
+    CTxDisplay( QWidget* parent = 0);
+    ~CTxDisplay();
+
+  public slots:
+    void abbruch();
+  protected:
+    void resizeEvent( QResizeEvent * );
+
+
+protected slots:
+    virtual void languageChange();
+  private:
+  signals:
+    void startRx();
+    void startTx();
+
+};
+
+#endif // CTXDISPLAY_H
